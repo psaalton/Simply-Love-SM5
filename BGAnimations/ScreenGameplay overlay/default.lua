nico-abram/Simply-Love-Etterna

Lane cover support

BGAnimations/ScreenGameplay overlay/default.lua
@@ -30,6 +30,7 @@ local t = Def.ActorFrame{
 for player in ivalues( GAMESTATE:GetHumanPlayers() ) do
 	SL[ToEnumShortString(player)].Stages.Stats[SL.Global.Stages.PlayedThisGame + 1] = {}
 
+	t[#t+1] = LoadActor("./LaneCover.lua", player)
 	t[#t+1] = LoadActor("./SubtractiveScoring.lua", player)
 	t[#t+1] = LoadActor("./PerColumnJudgmentTracking.lua", player)
 	t[#t+1] = LoadActor("./ReceptorArrowsPosition.lua", player)

psaalton/Simply-Love-SM5

BGAnimations/ScreenGameplay overlay/default.lua
@@ -38,6 +38,8 @@ for player in ivalues( GAMESTATE:GetHumanPlayers() ) do
 	-- FIXME: refactor PerColumnJudgmentTracking to not be inside this loop
 	--        the Lua input callback logic shouldn't be duplicated for each player
 	af[#af+1] = LoadActor("./PerColumnJudgmentTracking.lua", player)
+
+	af[#af+1] = LoadActor("./LaneCover.lua", player)
 end
 
 return af