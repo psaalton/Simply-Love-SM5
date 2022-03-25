34a7fd35facd1b3940eaba2121a947d85d1e7a8c
unknown <renne.brandt@iki.fi>
2022-03-25 11:45:31 +0200

    Hid new NC-scoring info behind Unicorn theme

--- BGAnimations/ScreenEvaluation common/PerPlayer/Upper/LetterGrade.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/Upper/LetterGrade.lua
@@ -20,17 +20,19 @@ if title == "D" then grade = "Grade_Tier99" end
 
 local t = Def.ActorFrame{}
 
-t[#t+1] = Def.Quad {
-	InitCommand=function(self)
-		self:diffuse(color("#101519")):zoomto(160, 150)
-		self:y(112)
-		self:x(70)
-		self:diffusealpha(0.40)
-		if player == PLAYER_1 then
-			self:x( self:GetX() * -1 )
+if ThemePrefs.Get("VisualStyle") == "Unicorn" then
+	t[#t+1] = Def.Quad {
+		InitCommand=function(self)
+			self:diffuse(color("#101519")):zoomto(160, 150)
+			self:y(112)
+			self:x(70)
+			self:diffusealpha(0.40)
+			if player == PLAYER_1 then
+				self:x( self:GetX() * -1 )
+			end
 		end
-	end
-}
+	}
+end
 
 t[#t+1] = LoadActor(THEME:GetPathG("", "_grades/"..grade..".lua"), playerStats)..{
 	InitCommand=function(self)
@@ -41,45 +43,45 @@ t[#t+1] = LoadActor(THEME:GetPathG("", "_grades/"..grade..".lua"), playerStats).
 }
 
 
-
-t[#t+1] = Def.Quad {
-	InitCommand=function(self)
-		self:diffuse(color("#101519")):zoomto(150, 30)
-		self:y(_screen.cy-71)
-		self:x(220)
-		self:diffusealpha(0.65)
-		if player == PLAYER_1 then
-			self:x( self:GetX() * -1 )
+if ThemePrefs.Get("VisualStyle") == "Unicorn" then
+	t[#t+1] = Def.Quad {
+		InitCommand=function(self)
+			self:diffuse(color("#101519")):zoomto(150, 30)
+			self:y(_screen.cy-71)
+			self:x(220)
+			self:diffusealpha(0.65)
+			if player == PLAYER_1 then
+				self:x( self:GetX() * -1 )
+			end
 		end
-	end
-}
+	}
 
-
-t[#t+1] = Def.BitmapText{
-    Font="Wendy/_wendy white",
-	Text="Normal Score",
-	InitCommand=function(self)
-		self:x(-210)
-		self:y(_screen.cy-79)
-		self:zoom(0.175)
-		if player == PLAYER_2 then
-			self:x( self:GetX() * -1 )
+	t[#t+1] = Def.BitmapText{
+		Font="Wendy/_wendy white",
+		Text="Normal Score",
+		InitCommand=function(self)
+			self:x(-210)
+			self:y(_screen.cy-79)
+			self:zoom(0.175)
+			if player == PLAYER_2 then
+				self:x( self:GetX() * -1 )
+			end
 		end
-    end
-}
+	}
 
-t[#t+1] = Def.BitmapText{
-    Font="Wendy/_wendy white",
-	Text=("%.2f"):format(CalculateITGScore(player)),
-	InitCommand=function(self)
-		self:zoom(0.175)
-		self:x(-210)
-		self:y(_screen.cy-65)
-		if player == PLAYER_2 then
-			self:x( self:GetX() * -1 )
+	t[#t+1] = Def.BitmapText{
+		Font="Wendy/_wendy white",
+		Text=("%.2f"):format(CalculateITGScore(player)),
+		InitCommand=function(self)
+			self:zoom(0.175)
+			self:x(-210)
+			self:y(_screen.cy-65)
+			if player == PLAYER_2 then
+				self:x( self:GetX() * -1 )
+			end
 		end
-    end
-}
+	}
+end
 
 
 
