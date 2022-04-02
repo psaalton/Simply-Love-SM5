7317f5bf745461e7fe8b9958516c39f4c591bd6a
Michael Sundqvist <michael.sundqvist1@gmail.com>
2022-04-02 12:28:18 +0300

    Revert "made some styling for the texts, mainly duration and stepcount for higher step counts"
    This reverts commit 42f1205fbadc80341c616c21b80da97d7941176c.

--- BGAnimations/ScreenEvaluation common/SessionStatistics.lua
+++ BGAnimations/ScreenEvaluation common/SessionStatistics.lua
@@ -19,24 +19,15 @@ for player in ivalues(Players) do
         end
     }
 
-    if stats.hours < 10 then
-        stats.hours = 0 .. stats.hours
-    end
-    if stats.minutes < 10 then
-        stats.minutes = 0 .. stats.minutes
-    end
-    if stats.notesHitThisGame > 9999 then
-        stats.notesHitThisGame = tonumber(string.format("%.1f", stats.notesHitThisGame/1000)) .. "k"
-    end
-
     sessionStats[#sessionStats+1] = LoadFont("Common Normal").. {
         Name="Stats",
-        Text=("%s:%s\nSongs: %s\nNotes: %s\nRestart streak: %s \nTotal restarts: %s"):format(
+        Text=("Songs: %s\nNotes: %s\n%sh %sm %ss\nSong restarted: %s \nTotal restarts: %s"):format(
 
-        stats.hours,
-        stats.minutes,
         stats.songsPlayedThisGame,
         stats.notesHitThisGame, 
+        stats.hours,
+        stats.minutes,
+        stats.seconds,
         SL.RestartCounter,
         stats.restartCountThisGame
 
