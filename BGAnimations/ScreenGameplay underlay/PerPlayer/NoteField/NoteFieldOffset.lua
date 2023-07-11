804b85fc33acadc187bdd17742f58a384c828677
Michael Sundqvist <michael.sundqvist1@gmail.com>
2023-07-11 22:20:22 +0300

    Set notefield offset x position based on player side

--- BGAnimations/ScreenGameplay underlay/PerPlayer/NoteField/NoteFieldOffset.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/NoteField/NoteFieldOffset.lua
@@ -4,8 +4,8 @@ local mods = SL[pn].ActiveModifiers
 
 return Def.Actor{
   OnCommand=function(self)
-    SCREENMAN:GetTopScreen():GetChild("Player"..pn):GetChild("NoteField"):addx(mods.NoteFieldOffsetX)
+    local multiplier = pn == "P1" and -1 or 1
+    SCREENMAN:GetTopScreen():GetChild("Player"..pn):GetChild("NoteField"):addx(mods.NoteFieldOffsetX * multiplier)
     SCREENMAN:GetTopScreen():GetChild("Player"..pn):GetChild("NoteField"):addy(mods.NoteFieldOffsetY)
   end,
 }
-
